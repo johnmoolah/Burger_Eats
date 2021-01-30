3695b3cefc3e180bd0b1e5553bdfcb295f3f562c
John Delizo <johndelizo99@gmail.com>
2021-01-29 20:00:08 -0800

    created api-routes to render index.handlebars

---
+++ routes/api-routes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+router.get("/", (req, res) => {
+    res.render("index", {burger_data: ["burger", "more"]});
+});
+
+module.exports = router;
